40706e3db6a2f970ce1ab3f3c22a065e87082b03
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-04-27 15:18:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,20 +39,21 @@ def sparse_accuracy_ignoring_last_label(y_true, y_pred):
     return K.sum(tf.to_float(legal_labels & K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))) / K.sum(tf.to_float(legal_labels))
 
 #os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" #If it doesn't work, uncomment this line; it should help.
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-path = r"C:\Users\Mattia\Desktop\Tesi\Dataset\Train-images"
-path1 =  r"C:\Users\Mattia\Desktop\Tesi\Dataset\Train-labels"
+#path = r"C:\Users\Mattia\Desktop\Tesi\Dataset\Train-images"
+#path1 =  r"C:\Users\Mattia\Desktop\Tesi\Dataset\Train-labels"
+
+path = r"/content/drive/MyDrive/Tesi/Dataset/Train_images"
+path1 = r"/content/drive/MyDrive/Tesi/Dataset/Train_labels"
 
 dir = os.listdir(path)
 dir1 = os.listdir(path1)
 
-minidir=dir[:3]
-minidir1=dir1[:3]
 image_list = []
 label_list = []
 
-for elem in minidir:
+for elem in dir:
     new_dir = os.path.join(path,elem)
 # read the image data using PIL
 image = Image.open(new_dir)
@@ -62,7 +63,7 @@ shape=image.shape
 print(shape)
 if image not in image_list : image_list.append(image)
 
-for lab in minidir1:
+for lab in dir1:
     new_dir1 = os.path.join(path1,lab)
 # read the image data using PIL
 label = Image.open(new_dir1)
@@ -76,12 +77,14 @@ if label not in label_list : label_list.append(label)
 list_train = image_list
 labels_train= label_list
 
+print(list_train)
+print(labels_train)
 
-model = rete(input_shape=shape,weight_decay=0., classes=5)
+#model = rete(input_shape=shape,weight_decay=0., classes=5)
 
-optimizer = SGD(learning_rate=0.01, momentum=0.9)
-loss_fn=softmax_sparse_crossentropy_ignoring_last_label
-metrics=[sparse_accuracy_ignoring_last_label]
+#optimizer = SGD(learning_rate=0.01, momentum=0.9)
+#loss_fn=softmax_sparse_crossentropy_ignoring_last_label
+#metrics=[sparse_accuracy_ignoring_last_label]
 
-model.compile(loss=loss_fn, optimizer=optimizer,metrics=metrics)
+#model.compile(loss=loss_fn, optimizer=optimizer,metrics=metrics)
 #model.fit(x = list_train,y=labels_train,epochs=2,steps_per_epoch=5)
