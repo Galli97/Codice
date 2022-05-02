cb4979133de5f3d49db69e53dce53589460a8b25
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-05-02 16:42:06 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def save_np_arrays(tmp1):
     with open('image_arrays.npy','wb') as f:
-        np.save(f,tmp1)
+        np.savetxt(f,tmp1)
 
 def get_np_arrays(file):
     with open(file,'rb') as f:
@@ -11,7 +11,7 @@ def get_np_arrays(file):
 
 def save_np_arrays_labels(tmp2):
     with open('label_arrays.npy','wb') as f:
-        np.save(f,tmp2)
+        np.savetxt(f,tmp2)
 
 ### datagenerator prepara i dati per il training
 def datagenerator(images,labels, batchsize, mode="train"):
