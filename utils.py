ed0ca97114522e589db9d12046b41d8dd2336144
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-05-04 16:08:42 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,19 +54,54 @@ def rotate(image):
     return rotated
 
 def augment(image_list,label_list):
-    n = random.randint(1,2)
-    tmp1a = np.empty((n, 64, 64, 3), dtype=np.uint8)
-    tmp2a = np.empty((n, 64, 64, 3), dtype=np.uint8)
+    n = random.randint(1,len(image_list)-1)
     for i in range (0,n):
         a = random.randint(0,len(image_list)-1)
-        image = cv2.imread(image_list[a])[:,:,[2,1,0]]
-        image = cv2.resize(image, (64,64))
-        new_image = rotate(image)
-        image_list.append(new_image)
-        label = cv2.imread(label_list[a])[:,:,[2,1,0]]
-        label = cv2.resize(label, (64,64))
-        new_label = rotate(label)
-        label_list.append(new_label)
-
+        if(a % 2 == 0):
+            image = cv2.imread(image_list[a])[:,:,[2,1,0]]
+            image = cv2.resize(image, (64,64))
+            new_image = rotate(image)
+            image_list.append(new_image)
+            label = cv2.imread(label_list[a])[:,:,[2,1,0]]
+            label = cv2.resize(label, (64,64))
+            new_label = rotate(label)
+            label_list.append(new_label)
+        if(a % 10 == 0):
+            image = cv2.imread(image_list[a])[:,:,[2,1,0]]
+            image = cv2.resize(image, (64,64))
+            new_image = brightness(image)
+            image_list.append(new_image)
+            label = cv2.imread(label_list[a])[:,:,[2,1,0]]
+            label = cv2.resize(label, (64,64))
+            new_label = brightness(label)
+            label_list.append(new_label)
+        if(a % 13 == 0):
+            image = cv2.imread(image_list[a])[:,:,[2,1,0]]
+            image = cv2.resize(image, (64,64))
+            new_image = saturate(image)
+            image_list.append(new_image)
+            label = cv2.imread(label_list[a])[:,:,[2,1,0]]
+            label = cv2.resize(label, (64,64))
+            new_label = saturate(label)
+            label_list.append(new_label)
+        if(a % 5 == 0):
+            image = cv2.imread(image_list[a])[:,:,[2,1,0]]
+            image = cv2.resize(image, (64,64))
+            new_image = flip(image)
+            image_list.append(new_image)
+            label = cv2.imread(label_list[a])[:,:,[2,1,0]]
+            label = cv2.resize(label, (64,64))
+            new_label = flip(label)
+            label_list.append(new_label)
+        if(a % 15 == 0):
+            image = cv2.imread(image_list[a])[:,:,[2,1,0]]
+            image = cv2.resize(image, (64,64))
+            new_image = grayscale(image)
+            image_list.append(new_image)
+            label = cv2.imread(label_list[a])[:,:,[2,1,0]]
+            label = cv2.resize(label, (64,64))
+            new_label = grayscale(label)
+            label_list.append(new_label)
+        
     return image_list,label_list,n
      
