c123679424a6de1b6c7abd5a3c1740ff4a3efb09
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-05-06 19:59:17 +0200

    lj

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ tmp2 = get_np_arrays(path1)          #recupero tmp2 dal file
 
 shape=(64,64,3)
 
-model = rete(input_shape=shape,weight_decay=0., classes=5)
+#model = rete(input_shape=shape,weight_decay=0., classes=5)
+model = DeeplabV3Plus(image_size=64, num_classes=NUM_CLASSES)
 
 EPOCHS=50
 train_set = int((tmp1.shape[0])*(2/3))
