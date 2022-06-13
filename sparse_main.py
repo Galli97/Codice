94f791902a306af52b68734c424fa9ceb5dfba9f
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-13 19:05:33 +0200

    Update sparse_main.py

--- sparse_main.py
+++ sparse_main.py
@@ -41,8 +41,8 @@ label_validation = tmp2[:train_set]
 
 shape=(64,64,3)
 
-model = rete(input_shape=shape,weight_decay=0.0001, classes=5)
-
+#model = rete(input_shape=shape,weight_decay=0.0001, classes=5)
+model = DeeplabV3Plus(input_shape=64,5)
 
 x_train = datagenerator(list_train,label_train,16)
 x_validation = datagenerator(list_validation,label_validation,16)
