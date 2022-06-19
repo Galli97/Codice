c0ab43c7b0afd46b1eff78b12455e216263bc89a
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-19 17:10:52 +0200

    Update sparse_label.py

--- sparse_label.py
+++ sparse_label.py
@@ -147,7 +147,7 @@ for j in range (N-A):
             # elif all(channels_xy==nullo):    #NULL
             #     new_label[i,n,0]=4
     #print(new_label.shape)
-    new_label = cv2.resize(new_label, (64,64))               #ridimension per combaciare con l'input
+    #new_label = cv2.resize(new_label, (64,64))               #ridimension per combaciare con l'input
     print('new_label: ', new_label.shape)
     tmp2[j] = new_label
     #print(tmp2.shape)
