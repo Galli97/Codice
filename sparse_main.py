52c239b5ef5fe24017114012b4c03b53fee93702
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-20 19:15:09 +0200

    Update sparse_main.py

--- sparse_main.py
+++ sparse_main.py
@@ -57,7 +57,7 @@ x_validation = datagenerator(list_validation,label_validation,BATCH)
 lr_base = 0.01 * (float(BATCH) / 16)
 optimizer = SGD(learning_rate=lr_base, momentum=0.)
 #optimizer=keras.optimizers.Adam(learning_rate=0.001)
-loss_fn =keras.losses.SparseCategoricalCrossentropy #softmax_sparse_crossentropy_ignoring_last_label
+loss_fn =keras.losses.SparseCategoricalCrossentropy() #softmax_sparse_crossentropy_ignoring_last_label
 model.compile(optimizer = optimizer, loss = loss_fn , metrics = ['accuracy'])#sparse_accuracy_ignoring_last_label])#,sample_weight_mode='temporal')
 
 ### AVVIO IL TRAINING #####
