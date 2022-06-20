d7477e9c0392b4bd1d0fc6b9cb5c1f9a44d20a75
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-20 18:02:43 +0200

    Update sparse_main.py

--- sparse_main.py
+++ sparse_main.py
@@ -42,7 +42,7 @@ label_validation = tmp2[train_set:]
 shape=(64,64,3)
 BATCH= 8
 EPOCHS=10
-steps = 50#int(train_set/EPOCHS)
+steps = 5#int(train_set/EPOCHS)
 #model = rete(input_shape=shape,weight_decay=0., classes=5)
 model = DeeplabV3Plus(image_size=64,num_classes=5)
 
