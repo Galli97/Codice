36d103ff29c1bdbac68b28c2c96ac7ef9dc45668
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-20 18:11:23 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -121,7 +121,7 @@ def sparse_accuracy_ignoring_last_label(y_true, y_pred):
     nb_classes = K.int_shape(y_pred)[-1]
     y_pred = K.reshape(y_pred, (-1, nb_classes))
 
-    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)),
+    y_true = K.one_hot(tf.cast(K.flatten(y_true), tf.int32),
                        nb_classes + 1)
     unpacked = tf.unstack(y_true, axis=-1)
     legal_labels = ~tf.cast(unpacked[-1], tf.bool)
