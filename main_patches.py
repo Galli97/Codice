07af0e6d89f8ba6f82b8365050b4e6fd7fea6208
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-28 15:35:33 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -125,8 +125,10 @@ model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, class
 
 # # Create a Dataset that includes sample weights
 # # (3rd element in the return tuple).
-x_train = tf.data.Dataset.from_tensors((list_train, label_train))
-x_validation = tf.data.Dataset.from_tensors((list_validation, label_validation))
+x_train = tf.data.Dataset.from_tensors((list_train, label_train))\
+        .batch(BATCH, drop_remainder=True)
+x_validation = tf.data.Dataset.from_tensors((list_validation, label_validation))\
+        .batch(BATCH, drop_remainder=True)
 
 x_train = x_train.map(add_sample_weights)
 x_validation = x_validation.map(add_sample_weights)
