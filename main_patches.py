b617e53495436a0d984dff34e9f85316989e4e1c
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-28 18:14:21 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -166,7 +166,7 @@ model.compile(optimizer = optimizer, loss = loss_fn , metrics =[sparse_accuracy_
 
 ### AVVIO IL TRAINING #####
 model.summary()
-history = model.fit(x = x_train,batch_size = BATCH, steps_per_epoch=steps,epochs=EPOCHS,validation_data=x_validation,validation_step=steps,validation_batch_size=BATCH)
+history = model.fit(x = x_train,batch_size = BATCH, steps_per_epoch=steps,epochs=EPOCHS,validation_data=x_validation,validation_steps=steps,validation_batch_size=BATCH)
 model.save('model.h5')
 
 plt.plot(history.history["loss"])
