e79f870b51c188dc0abed7d00ee355dcc1ee6b55
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-29 18:12:04 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -39,7 +39,7 @@ print(tmp1.shape)
 # print('999: ',tmp1[999])
 # print('0: ',tmp1[0])
 # print('4000: ',tmp1[4000])
-tmp1=tmp1[:100]
+tmp1=tmp1[:200]
 # print(tmp1.shape)
 
 
@@ -50,7 +50,7 @@ print(tmp2.shape)
 # print('999: ',tmp2[999])
 # print('0: ',tmp2[0])
 # print('3050: ',tmp2[3050])
-tmp2=tmp2[:100]
+tmp2=tmp2[:200]
 # print(tmp2.shape)
 
 
