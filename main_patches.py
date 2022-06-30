19b71b79603f548af6b88bf66223fd3518461658
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-06-30 15:26:25 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -98,7 +98,7 @@ EPOCHS = 10
 steps = int(train_set/EPOCHS)
 weight_decay = 0.0001/2
 batch_shape=(BATCH,64,64,1)
-model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
+model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=batch_shape, classes=5)
 
 #model = DeeplabV3Plus(image_size=64,num_classes=5)
 
