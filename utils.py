28101ab3709d9ec896a514a2e9ee68f2b408fc99
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-02 13:31:04 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -331,6 +331,7 @@ def dice_coeff(y_true, y_pred):
     smooth = 1.
     # Flatten
     y_true_f = tf.reshape(y_true, [-1])
+    y_pred = tf.math.argmax(y_pred, axis=-1)
     y_pred_f = tf.reshape(y_pred, [-1])
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
     score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
