baade5f91304880eaba14c7645403be3ee615574
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-02 13:08:35 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -306,11 +306,11 @@ def dice_coef(y_true, y_pred,smooth=1):
   return dice
 
 def iou_coef(y_true, y_pred):
-  y_true = tf.cast(y_true, tf.float32)
-  y_pred = tf.cast(y_pred, tf.float32)
   y_true = K.flatten(y_true)
   y_pred = tf.math.argmax(y_pred)
   y_pred = K.flatten(y_pred)
+  y_true = tf.cast(y_true, tf.float32)
+  y_pred = tf.cast(y_pred, tf.float32)
   intersection = K.sum(y_true * y_pred)
   union = K.sum(y_true)+K.sum(y_pred)-intersection
   iou = intersection / union 
