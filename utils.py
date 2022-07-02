23b52a23a60c1c211df51011d896d9aebf58b2d6
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-02 13:09:57 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -310,7 +310,7 @@ def iou_coef(y_true, y_pred):
   y_pred = K.flatten(y_pred)
   y_true = tf.cast(y_true, tf.float32)
   y_pred = tf.cast(y_pred, tf.float32)
-  y_pred = tf.math.argmax(y_pred)
+  #y_pred = tf.math.argmax(y_pred)
   intersection = K.sum(y_true * y_pred)
   union = K.sum(y_true)+K.sum(y_pred)-intersection
   iou = intersection / union 
