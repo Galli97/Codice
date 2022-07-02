eb8477093c1ee73d72fadd497af7a46085027952
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-02 13:28:24 +0200

    k

--- utils.py
+++ utils.py
@@ -334,7 +334,9 @@ def dice_coeff(y_true, y_pred):
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
     score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
     return score
-
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
 def iou_coef(y_true, y_pred):
   y_true = K.flatten(y_true)
   y_pred = K.flatten(y_pred)
