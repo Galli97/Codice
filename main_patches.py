a7649c65908e4c78375dda2644d5c08091de51e5
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-03 16:02:13 +0200

    ,

--- main_patches.py
+++ main_patches.py
@@ -69,8 +69,8 @@ print('label_validation: ',label_validation.shape)
 shape=(64,64,3)
 print(shape)
 BATCH = 8
-EPOCHS = 50 
-steps = 5#int(train_set/EPOCHS)
+EPOCHS = 100 
+steps = 6#int(train_set/EPOCHS)
 weight_decay = 0.0001/2
 batch_shape=(BATCH,64,64,1)
 #model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
