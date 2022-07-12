af98420491761977952facaf53ae5a0fb85721e6
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-12 15:21:54 +0200

    k

--- rete.py
+++ rete.py
@@ -42,6 +42,25 @@ def upsample(filters, size, apply_dropout=False):
 
   return result
 
+class BilinearInitializer(keras.initializers.Initializer):
+    '''Initializer for Conv2DTranspose to perform bilinear interpolation on each channel.'''
+    def __call__(self, shape, dtype=None, **kwargs):
+        kernel_size, _, filters, _ = shape
+        arr = np.zeros((kernel_size, kernel_size, filters, filters))
+        ## make filter that performs bilinear interpolation through Conv2DTranspose
+        upscale_factor = (kernel_size+1)//2
+        if kernel_size % 2 == 1:
+            center = upscale_factor - 1
+        else:
+            center = upscale_factor - 0.5
+        og = np.ogrid[:kernel_size, :kernel_size]
+        kernel = (1-np.abs(og[0]-center)/upscale_factor) * \
+                 (1-np.abs(og[1]-center)/upscale_factor) # kernel shape is (kernel_size, kernel_size)
+        for i in range(filters):
+            arr[..., i, i] = kernel
+        return tf.convert_to_tensor(arr, dtype=dtype)
+
+
 # class UpSampling2D(Layer):
 #     def __init__(self, size=(1, 1), target_size=None, data_format='default', **kwargs):
 #         if data_format == 'default':
@@ -233,16 +252,21 @@ def rete_vgg16(img_size=None, weight_decay=0., batch_momentum=0.9, batch_shape=N
     x = Conv2D(1024, (3, 3), activation='relu', padding='same', name='fc2', kernel_regularizer=l2(weight_decay))(x)
     #x = Dropout(0.5)(x)
     x = Conv2D(classes, (3, 3), activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
-    x = tf.keras.layers.UpSampling2D(16,interpolation='bilinear')(x)
-    x = Activation('softmax')(x)
+    x = keras.layers.Conv2DTranspose(filters=classes, kernel_size=(16,16), strides=(16,16),
+                                     padding='same', use_bias=False, activation='softmax',
+                                     kernel_initializer=BilinearInitializer(),
+                                     kernel_regularizer=l2(weight_decay),
+                                     name='fc3')(x)
+    # x = tf.keras.layers.UpSampling2D(16,interpolation='bilinear')(x)
+    # x = Activation('softmax')(x)
     model = Model(inputs=vggmodel.input, outputs=x)
 
     return model
 
-def rete_vgg16_dilation(image_size=None, weight_decay=0., batch_momentum=0.9, batch_shape=None, classes=5):
+def rete_vgg16_dilation(img_size=None, weight_decay=0., batch_momentum=0.9, batch_shape=None, classes=5):
     
     
-    vggmodel = tf.keras.applications.vgg16.VGG16(input_shape=image_size, weights='imagenet',include_top=False)
+    vggmodel = tf.keras.applications.vgg16.VGG16(input_shape=img_size, weights='imagenet',include_top=False)
 
     vggmodel = Sequential(vggmodel.layers[:-8])
     for layer in vggmodel.layers:        
@@ -269,65 +293,3 @@ def rete_vgg16_dilation(image_size=None, weight_decay=0., batch_momentum=0.9, ba
 
     return model
 
-############ DEEPLABV3+ IMPLEMENTATA NELL'ESEMPIO DI TENSORFLOW#######
-def convolution_block(
-    block_input,
-    num_filters=256,
-    kernel_size=3,
-    dilation_rate=1,
-    padding="same",
-    use_bias=False,
-):
-    x = layers.Conv2D(
-        num_filters,
-        kernel_size=kernel_size,
-        dilation_rate=dilation_rate,
-        padding="same",
-        use_bias=use_bias,
-        kernel_initializer=keras.initializers.HeNormal(),
-    )(block_input)
-    x = layers.BatchNormalization()(x)
-    return tf.nn.relu(x)
-
-
-def DilatedSpatialPyramidPooling(dspp_input):
-    dims = dspp_input.shape
-    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
-    x = convolution_block(x, kernel_size=1, use_bias=True)
-    out_pool = layers.UpSampling2D(
-        size=(dims[-3] // x.shape[1], dims[-2] // x.shape[2]), interpolation="bilinear",
-    )(x)
-
-    out_1 = convolution_block(dspp_input, kernel_size=1, dilation_rate=1)
-    out_6 = convolution_block(dspp_input, kernel_size=3, dilation_rate=6)
-    out_12 = convolution_block(dspp_input, kernel_size=3, dilation_rate=12)
-    out_18 = convolution_block(dspp_input, kernel_size=3, dilation_rate=18)
-
-    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
-    output = convolution_block(x, kernel_size=1)
-    return output
-
-def DeeplabV3Plus(image_size, num_classes):
-    model_input = keras.Input(shape=(image_size, image_size, 3))
-    resnet50 = keras.applications.ResNet50(
-        weights="imagenet", include_top=False, input_tensor=model_input
-    )
-    x = resnet50.get_layer("conv4_block6_2_relu").output
-    x = DilatedSpatialPyramidPooling(x)
-
-    input_a = layers.UpSampling2D(
-        size=(image_size // 4 // x.shape[1], image_size // 4 // x.shape[2]),
-        interpolation="bilinear",
-    )(x)
-    input_b = resnet50.get_layer("conv2_block3_2_relu").output
-    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
-
-    x = layers.Concatenate(axis=-1)([input_a, input_b])
-    x = convolution_block(x)
-    x = convolution_block(x)
-    x = layers.UpSampling2D(
-        size=(image_size // x.shape[1], image_size // x.shape[2]),
-        interpolation="bilinear",
-    )(x)
-    model_output = layers.Conv2D(num_classes, kernel_size=(1, 1), padding="same")(x)
-    return keras.Model(inputs=model_input, outputs=model_output)
