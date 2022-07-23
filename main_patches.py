d0a330a30f09488265effe8b47b9ea5c4b700fcb
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-23 19:21:20 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -102,8 +102,8 @@ weight_decay = 0.0001/2
 batch_shape=(BATCH,SHAPE,SHAPE,3)
 #model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
 tf.keras.backend.set_image_data_format('channels_last')
-#model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
-model = rete_vgg16_dilation(img_size=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
+model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
+#model = rete_vgg16_dilation(img_size=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
 input_shape = (SHAPE, SHAPE, 3)
 #model = build_vgg16_unet(input_shape,weight_decay=weight_decay, classes=5)
 
