afecb9dddfd10ae8171e489e6d74818a9295ee31
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-24 22:59:07 +0200

    Update rete.py

--- rete.py
+++ rete.py
@@ -267,7 +267,7 @@ def build_vgg16_unet(input_shape,weight_decay=0.,classes=5):
     """ Decoder """
            
     d1 = decoder_block(b3, s4, classes)                     ## (64 x 64)
-    d2 = decoder_block(d1, s3, 128)                     ## (128 x 128)
+    d2 = decoder_block(d1, s3, 64)                     ## (128 x 128)
     # d3 = decoder_block(d2, s2, 128)                     ## (256 x 256)
     # d4 = decoder_block(d3, s1, 64)                      ## (512 x 512)
 
