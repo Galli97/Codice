a8a1b315da30c950078ad9a18d2dbc1f1f64270e
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-24 17:56:35 +0200

    Update rete.py

--- rete.py
+++ rete.py
@@ -117,11 +117,14 @@ def rete(input_shape=None, weight_decay=0., batch_shape=None, classes=5):
     x = Dropout(0.5)(x)
     x = Conv2D(1024, (3, 3), activation='relu', padding='same', name='fc4', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.75)(x)
-    x = Conv2D(classes, (1, 1), activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(classes, (3, 3), activation='relu', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
     
     x = tf.keras.layers.UpSampling2D(16,interpolation='bilinear')(x)
 
-    x = Activation('softmax')(x)
+    x = Conv2D(classes, 1, activation='softmax')(x)
+    
+    
+    #x = Activation('softmax')(x)
    
     model = Model(img_input, x)
 
