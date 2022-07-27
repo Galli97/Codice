6c1961889e2db8ed362223a2f5081dd20778bd39
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-07-27 17:31:37 +0200

    km

--- rete.py
+++ rete.py
@@ -258,9 +258,9 @@ def build_vgg16_unet(input_shape,weight_decay=0.,classes=5):
     b1_pooling = MaxPooling2D((2, 2), strides=(2, 2),padding='same', name='block5_pool')(b1)
   
     b2 = Conv2D(1024, (3, 3), activation='relu', padding='same',dilation_rate=(12,12), name='fc5', kernel_regularizer=l2(weight_decay))(b1_pooling)
-    x = Dropout(0.75)(x)
+    x = Dropout(0.6)(x)
     b2 = Conv2D(1024, (3, 3), activation='relu', padding='same', name='fc6', kernel_regularizer=l2(weight_decay))(b2)
-    x = Dropout(0.75)(x)
+    x = Dropout(0.6)(x)
     b3 = Conv2D(classes, (3, 3), activation='relu', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(b2)
     #x = tf.keras.layers.UpSampling2D(32,interpolation='bilinear')(x)
     
