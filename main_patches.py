8a03fc25b175cefb4a3f30711fec80c159796a9f
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-08-03 13:58:58 +0200

    kj

--- main_patches.py
+++ main_patches.py
@@ -38,16 +38,16 @@ session = InteractiveSession(config=config)
 # path7 = r"/content/drive/MyDrive/Tesi/Dataset64/final_labels_4.npy"
 
 #128x128
-# path = r"/content/drive/MyDrive/Tesi/Dataset128/final_images.npy"
-# path1 = r"/content/drive/MyDrive/Tesi/Dataset128/final_labels.npy"
-# path2 = r"/content/drive/MyDrive/Tesi/Dataset128/final_images_2.npy"
-# path3 = r"/content/drive/MyDrive/Tesi/Dataset128/final_labels_2.npy"
-# path4 = r"/content/drive/MyDrive/Tesi/Dataset128/final_images_3.npy"
-# path5 = r"/content/drive/MyDrive/Tesi/Dataset128/final_labels_3.npy"
+path = r"/content/drive/MyDrive/Tesi/Dataset128/final_images.npy"
+path1 = r"/content/drive/MyDrive/Tesi/Dataset128/final_labels.npy"
+path2 = r"/content/drive/MyDrive/Tesi/Dataset128/final_images_2.npy"
+path3 = r"/content/drive/MyDrive/Tesi/Dataset128/final_labels_2.npy"
+path4 = r"/content/drive/MyDrive/Tesi/Dataset128/final_images_3.npy"
+path5 = r"/content/drive/MyDrive/Tesi/Dataset128/final_labels_3.npy"
 
 #128x128_2
-path = r"/content/drive/MyDrive/Tesi/Dataset128_2/final_images.npy"
-path1 = r"/content/drive/MyDrive/Tesi/Dataset128_2/final_labels.npy"
+# path = r"/content/drive/MyDrive/Tesi/Dataset128_2/final_images.npy"
+# path1 = r"/content/drive/MyDrive/Tesi/Dataset128_2/final_labels.npy"
 # path2 = r"/content/drive/MyDrive/Tesi/Dataset128_2/final_images_2.npy"
 # path3 = r"/content/drive/MyDrive/Tesi/Dataset128_2/final_labels_2.npy"
 
@@ -65,17 +65,17 @@ print('tmp1: ',tmp1.shape)
 tmp2 = get_np_arrays(path1)          #recupero tmp2 dal file
 print('tmp2: ',tmp2.shape)
 
-# tmp3 = get_np_arrays(path2)          #recupero tmp1 dal file 
-# print('tmp3: ',tmp3.shape)
+tmp3 = get_np_arrays(path2)          #recupero tmp1 dal file 
+print('tmp3: ',tmp3.shape)
 
-# tmp4 = get_np_arrays(path3)          #recupero tmp2 dal file
-# print('tmp4: ',tmp4.shape)
+tmp4 = get_np_arrays(path3)          #recupero tmp2 dal file
+print('tmp4: ',tmp4.shape)
 
-# tmp5 = get_np_arrays(path4)          #recupero tmp1 dal file 
-# print('tmp5: ',tmp5.shape)
+tmp5 = get_np_arrays(path4)          #recupero tmp1 dal file 
+print('tmp5: ',tmp5.shape)
 
-# tmp6 = get_np_arrays(path5)          #recupero tmp2 dal file
-# print('tmp6: ',tmp6.shape)
+tmp6 = get_np_arrays(path5)          #recupero tmp2 dal file
+print('tmp6: ',tmp6.shape)
 
 # tmp7 = get_np_arrays(path6)          #recupero tmp1 dal file 
 # print('tmp5: ',tmp7.shape)
@@ -84,11 +84,11 @@ print('tmp2: ',tmp2.shape)
 # print('tmp6: ',tmp8.shape)
 
 
-# tmp1=np.concatenate((tmp1,tmp3))#,tmp5))#,tmp7))
-# tmp2=np.concatenate((tmp2,tmp4))#,tmp6))#,tmp8))
+tmp1=np.concatenate((tmp1,tmp3))#,tmp5))#,tmp7))
+tmp2=np.concatenate((tmp2,tmp4))#,tmp6))#,tmp8))
 
-# print('tmp1_new: ',tmp1.shape)
-# print('tmp2_new: ',tmp2.shape)
+print('tmp1_new: ',tmp1.shape)
+print('tmp2_new: ',tmp2.shape)
 
 #################
 class Augment(tf.keras.layers.Layer):
@@ -103,6 +103,7 @@ class Augment(tf.keras.layers.Layer):
     labels = self.augment_labels(labels)
     return inputs, labels
 ####################
+
 # print(tmp1.shape)
 # print(tmp2.shape)
 # #### PRENDO UNA PARTE DEL DATASET (20%) E LO UTILIZZO PER IL VALIDATION SET #####
