ca7c6f16b5623fde8e2e003575ff06c52fa9304f
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-08-05 17:38:21 +0200

    xv

--- rete.py
+++ rete.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from keras.layers import *
 from tensorflow import keras
 from keras import layers
-from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import ResNet50, ResNet101
 from tensorflow.keras import Input
 from keras import Model,Sequential
 from keras.initializers import RandomNormal
@@ -153,16 +153,16 @@ def get_weights_path_vgg16():
     return weights_path
 
 
-def rete_Resnet50(img_size=None, weight_decay=0., batch_momentum=0.9, batch_shape=None, classes=5):
+def rete_Resnet101(img_size=None, weight_decay=0., batch_momentum=0.9, batch_shape=None, classes=5):
     
     model_input = keras.Input(shape=(img_size, img_size, 3))
 
-    res_model = ResNet50(weights='imagenet',include_top=False,input_tensor=model_input)
+    res_model = ResNet101(weights='imagenet',include_top=False,input_tensor=model_input)
 
     #res_model = Sequential(res_model.layers[:-4])
-    for layer in res_model.layers[:-4]:        
-        layer.trainable = False
-        print(layer.name)
+    # for layer in res_model.layers[:-4]:        
+    #     layer.trainable = False
+    #     print(layer.name)
     x = res_model.output
     
     x = Conv2D(classes, (3, 3), dilation_rate=(2, 2), kernel_initializer='normal', activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
