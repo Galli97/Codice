d45520fea79073c24b2254e8259189246fccb02c
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-08-05 15:57:45 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -152,7 +152,7 @@ tf.keras.backend.set_image_data_format('channels_last')
 #model = build_vgg16_unet(input_shape,weight_decay=weight_decay, classes=5)
 #model = DeeplabV3Plus(image_size=128, num_classes=5)
 model = rete_Resnet50(img_size=128,weight_decay=weight_decay,batch_shape=None, classes=5)
-model = AtrousFCN_Resnet50_16s(input_shape = shape, weight_decay=weight_decay, batch_momentum=0.9, batch_shape=None, classes=5):
+model = AtrousFCN_Resnet50_16s(input_shape = shape, weight_decay=weight_decay, batch_momentum=0.9, batch_shape=None, classes=5)
 
 ##### USO DATAGENERATOR PER PREPARARE I DATI DA MANDARE NELLA RETE #######
 # x_train = datagenerator(list_train,label_train,BATCH)
