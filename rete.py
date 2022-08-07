2a62f72a81abf0340ed37aeb2df0b627300ec18b
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-08-07 14:29:46 +0200

    Update rete.py

--- rete.py
+++ rete.py
@@ -292,6 +292,7 @@ def build_vgg16_unet(input_shape,weight_decay=0.,classes=5):
     b3 = Conv2D(classes, (3, 3), activation='relu', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(b2)
     #x = tf.keras.layers.UpSampling2D(32,interpolation='bilinear')(x)
     
+    
     """ Decoder """
            
     d1 = decoder_block(b3, s4, classes)                 ## (64 x 64)
