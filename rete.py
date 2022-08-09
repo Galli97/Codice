0c2c3006f4e502bda7b744d951b500b907e16601
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-08-09 17:24:24 +0200

    sacv

--- rete.py
+++ rete.py
@@ -115,16 +115,16 @@ def rete(input_shape=None, weight_decay=0., batch_shape=None, classes=5):
     # Block 5
     x = Conv2D(1024, (3, 3), activation='relu', padding='same',dilation_rate=(8,8), name='fc3', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(1024, (3, 3), activation='relu', padding='same',dilation_rate=(2,2), name='fc4', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(1024, (3, 3), activation='relu', padding='same', name='fc4', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(classes, (3, 3),  kernel_initializer='normal',dilation_rate=(2,2), activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(classes, (3, 3),  kernel_initializer='normal',dilation_rate=(4,4), activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
     #x = Conv2D(classes, (1, 1),kernel_initializer='he_normal', activation='linear', padding='valid', kernel_regularizer=l2(weight_decay))(x)
     
     x = tf.keras.layers.UpSampling2D(16,interpolation='bilinear')(x)
 
-    x = Conv2D(classes, 1,strides=(1, 1), activation='softmax', padding='valid',kernel_regularizer=l2(weight_decay))(x)
+    #x = Conv2D(classes, 1,strides=(1, 1), activation='softmax', padding='valid',kernel_regularizer=l2(weight_decay))(x)
     
-    #x = Activation('softmax')(x)
+    x = Activation('softmax')(x)
    
     model = Model(img_input, x)
     # for layer in model.layers[:-15]:        
