e5b406e732a1ecf2aea30089a74dfef1f8d288db
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-08-17 13:40:12 +0200

    Update rete.py

--- rete.py
+++ rete.py
@@ -88,10 +88,10 @@ def rete(input_shape=None, weight_decay=0., batch_shape=None, classes=5):
     x = Dropout(0.5)(x)
     x = Conv2D(1024, (3, 3), activation='relu', padding='same', name='fc4', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(classes, (3, 3),  kernel_initializer='normal',dilation_rate=(2,2), activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
-    #x = Conv2D(classes, (1, 1),kernel_initializer='he_normal', activation='linear', padding='valid', kernel_regularizer=l2(weight_decay))(x)
+    #x = Conv2D(classes, (3, 3),  kernel_initializer='normal',dilation_rate=(2,2), activation='linear', padding='same', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(classes, (1, 1),kernel_initializer='he_normal', activation='linear', padding='valid', kernel_regularizer=l2(weight_decay))(x)
     
-    x = tf.keras.layers.UpSampling2D(16,interpolation='bilinear')(x)
+    x = tf.keras.layers.UpSampling2D(32,interpolation='bilinear')(x)
 
     #x = Conv2D(classes, 1,strides=(1, 1), activation='softmax', padding='valid',kernel_regularizer=l2(weight_decay))(x)
     
