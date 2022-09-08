9438abc18efa1b79306de541ce7904c482a43ae6
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-09-08 13:18:48 +0200

    k

--- read_patcehs.py
+++ read_patcehs.py
@@ -61,6 +61,8 @@ from keras.preprocessing.image import ImageDataGenerator
 
 path = r"C:\Users\Mattia\Desktop\DatasetSoil\final_images.npy"
 path1 = r"C:\Users\Mattia\Desktop\DatasetSoil\final_labels.npy"
+path2 = r"C:\Users\Mattia\Desktop\DatasetSoil\final_images_2.npy"
+path3 = r"C:\Users\Mattia\Desktop\DatasetSoil\final_labels_2.npy"
 
 # path = r"C:\Users\Mattia\Documenti\Github\Codice\final_images.npy"
 # path1 =  r"C:\Users\Mattia\Documenti\Github\Codice\final_labels.npy"
@@ -74,11 +76,11 @@ print('tmp1: ',tmp1.shape)
 tmp2 = get_np_arrays(path1)          #recupero tmp2 dal file
 print('tmp2: ',tmp2.shape)
 
-# tmp3 = get_np_arrays(path2)          #recupero tmp1 dal file 
-# print('tmp3: ',tmp3.shape)
+tmp3 = get_np_arrays(path2)          #recupero tmp1 dal file 
+print('tmp3: ',tmp3.shape)
 
-# tmp4 = get_np_arrays(path3)          #recupero tmp2 dal file
-# print('tmp4: ',tmp4.shape)
+tmp4 = get_np_arrays(path3)          #recupero tmp2 dal file
+print('tmp4: ',tmp4.shape)
 
 # tmp5 = get_np_arrays(path4)          #recupero tmp1 dal file 
 # print('tmp5: ',tmp5.shape)
@@ -105,8 +107,8 @@ print('tmp2: ',tmp2.shape)
 # print('tmp12: ',tmp12.shape)
 
 
-# tmp1=np.concatenate((tmp1,tmp3,tmp5,tmp7,tmp9,tmp11))
-# tmp2=np.concatenate((tmp2,tmp4,tmp6,tmp8,tmp10,tmp12))
+tmp1=np.concatenate((tmp1,tmp3))##,tmp5,tmp7,tmp9,tmp11))
+tmp2=np.concatenate((tmp2,tmp4))##,tmp6,tmp8,tmp10,tmp12))
 
 print('tmp1_new: ',tmp1.shape)
 print('tmp2_new: ',tmp2.shape)
