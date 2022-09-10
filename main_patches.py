5a71f7060a65731f3d4b1dd67106b94aec9838f3
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-09-10 19:56:25 +0200

    sdv

--- main_patches.py
+++ main_patches.py
@@ -186,7 +186,7 @@ BATCH = 32
 EPOCHS = 250
 #steps = int(train_set/(EPOCHS))
 steps = int(np.ceil(train_set/ float(BATCH)))
-weight_decay =0.0005#0.0001/2 
+weight_decay =0.0001#0.0001/2 
 
 #steps_val = int(np.ceil(len(list_validation)/ float(BATCH)+1))
 #steps_val = int(len(list_validation)/EPOCHS)
@@ -197,8 +197,8 @@ tf.keras.backend.set_image_data_format('channels_last')
 
 #model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
 #model = rete_vgg16_dilation(img_size=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
-model = build_vgg16_unet(input_shape,weight_decay=weight_decay, classes=5)
-#model = rete_Resnet101(img_size=128,weight_decay=weight_decay,batch_shape=None, classes=5)
+#model = build_vgg16_unet(input_shape,weight_decay=weight_decay, classes=5)
+model = rete_Resnet101(img_size=128,weight_decay=weight_decay,batch_shape=None, classes=5)
 #model = AtrousFCN_Resnet50_16s(input_shape = shape, weight_decay=weight_decay, batch_momentum=0.95, batch_shape=None, classes=5)
 #model=mobileNET(shape=input_shape,weight_decay=weight_decay)
 #model=mobile(shape=input_shape,weight_decay=weight_decay)
@@ -234,9 +234,11 @@ lr_base = 0.01 * (float(BATCH) / 16)
 def lr_scheduler(epoch):
   
     # drops as progression proceeds, good for sgd
-    if epoch > 0.7 * EPOCHS:
+    if epoch > 0.8 * EPOCHS:
+        lr = 0.0005
+    elif epoch > 0.5 * EPOCHS:
         lr = 0.001
-    elif epoch > 0.3 * EPOCHS:
+    elif epoch > 0.2 * EPOCHS:
         lr = 0.005
     else:
         lr = 0.01
