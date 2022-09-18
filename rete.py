f24c5ac8304382c9c905597cf0764a896e3e1388
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-09-18 12:31:07 +0200

    Update rete.py

--- rete.py
+++ rete.py
@@ -265,9 +265,9 @@ def build_vgg16_unet(input_shape,weight_decay=0.,classes=5):
     """ Decoder """
            
     d1 = decoder_block(b3, s4, classes)                 ## (64 x 64)
-    d2 = decoder_block(d1, s3, 256)                     ## (128 x 128)
-    d3 = decoder_block(d2, s2, 128)                     ## (256 x 256)
-    d4 = decoder_block(d3, s1, 64)                      ## (512 x 512)
+    d2 = decoder_block(d1, s3, 128)                     ## (128 x 128)
+    d3 = decoder_block(d2, s2, 64)                     ## (256 x 256)
+    d4 = decoder_block(d3, s1, 32)                      ## (512 x 512)
 
     """ Output """
     outputs = Conv2D(5, 1, padding="valid", activation="softmax",kernel_regularizer=l2(weight_decay))(d4)
