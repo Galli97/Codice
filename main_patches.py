df9900a10aab4e76cfaaec581b7db1828005cdac
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-09-18 13:27:50 +0200

    Update main_patches.py

--- main_patches.py
+++ main_patches.py
@@ -198,7 +198,7 @@ BATCH = 32
 EPOCHS = 250
 #steps = int(train_set/(EPOCHS))
 steps = int(np.ceil(train_set/ float(BATCH)))
-weight_decay =0.0001 #0.0001/2 
+weight_decay =0.0005 #0.0001/2 
 
 #steps_val = int(np.ceil(len(list_validation)/ float(BATCH)+1))
 #steps_val = int(len(list_validation)/EPOCHS)
@@ -210,9 +210,9 @@ tf.keras.backend.set_image_data_format('channels_last')
 #model = rete(input_shape=shape,weight_decay=weight_decay,batch_shape=None, classes=5)
 #model = build_vgg16_unet(input_shape,weight_decay=weight_decay, classes=5)
 #model = rete_Resnet101(img_size=128,weight_decay=weight_decay,batch_shape=None, classes=5)
-model = AtrousFCN_Resnet50_16s(input_shape = shape, weight_decay=weight_decay, batch_momentum=0.95, batch_shape=None, classes=5)
+#model = AtrousFCN_Resnet50_16s(input_shape = shape, weight_decay=weight_decay, batch_momentum=0.95, batch_shape=None, classes=5)
 #model=mobile(shape=input_shape,weight_decay=weight_decay)
-#model = DeeplabV3Plus(image_size=SHAPE, num_classes=5)
+model = DeeplabV3Plus(image_size=SHAPE, num_classes=5)
 
 ##### USO DATAGENERATOR PER PREPARARE I DATI DA MANDARE NELLA RETE #######
 # x_train = datagenerator(list_train,label_train,BATCH)
