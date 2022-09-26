346b7628251933aac09b7bfc74715c7cee50d88d
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-09-26 20:44:22 +0200

    kn

--- evaluation.py
+++ evaluation.py
@@ -33,16 +33,16 @@ from keras.preprocessing.image import ImageDataGenerator
 # path1 =  r"C:\Users\Mattia\Desktop\Dataset_1\Test255\label_patches_TEST.npy"
 
 #128-Resized 255 Norm
-# path = r"C:\Users\Mattia\Desktop\Resized_Test\Test255\image_patches_TEST.npy"
-# path1 =  r"C:\Users\Mattia\Desktop\Resized_Test\Test255\label_patches_TEST.npy"
+path = r"C:\Users\Mattia\Desktop\Resized_Test\Test255\image_patches_TEST.npy"
+path1 =  r"C:\Users\Mattia\Desktop\Resized_Test\Test255\label_patches_TEST.npy"
 
 #128-Resized 510 norm
 # path = r"C:\Users\Mattia\Desktop\Resized_Test\image_patches_TEST.npy"
 # path1 =  r"C:\Users\Mattia\Desktop\Resized_Test\label_patches_TEST.npy"
 
 #128-all Test set gold 255 norm
-path = r"C:\Users\Mattia\Desktop\image_patches_TEST.npy"
-path1 =  r"C:\Users\Mattia\Desktop\label_patches_TEST.npy"
+# path = r"C:\Users\Mattia\Desktop\image_patches_TEST.npy"
+# path1 =  r"C:\Users\Mattia\Desktop\label_patches_TEST.npy"
 
 #128-all Test set gold 255 norm COLAB
 # path = r"/content/drive/MyDrive/Tesi/image_patches_TEST.npy"
@@ -136,11 +136,11 @@ matrix_nonull=matrix[1:4,1:4]
 # soil_pixels= soil_count 
 
 #128 tutte le 322 croppate (20000 immagini) delle merged
-null_pixels =  220661911
-bedrock_pixels= 26192939
-sand_pixels= 30639905
-bigrock_pixels= 77698
-soil_pixels= 60069019
+# null_pixels =  220661911
+# bedrock_pixels= 26192939
+# sand_pixels= 30639905
+# bigrock_pixels= 77698
+# soil_pixels= 60069019
 
 #128 1500
 # null_pixels =  9663358
@@ -157,11 +157,11 @@ soil_pixels= 60069019
 # soil_pixels= 4215266
 
 #128 resized
-# null_pixels =  12314115
-# bedrock_pixels =  5000050
-# sand_pixels =  863492
-# bigrock_pixels =  41557
-# soil_pixels = 6356786
+null_pixels =  12314115
+bedrock_pixels =  5000050
+sand_pixels =  863492
+bigrock_pixels =  41557
+soil_pixels = 6356786
 
 
 matrix2 = np.array([[matrix[0]*100/null_pixels],[matrix[1]*100/bedrock_pixels], [matrix[2]*100/sand_pixels],[matrix[3]*100/bigrock_pixels], [matrix[4]*100/soil_pixels]])
