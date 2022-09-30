2db296e51eecc918c354be8e9bafa4319f1fd5a2
Galli97 <galli.1753274@studenti.uniroma1.it>
2022-09-30 14:47:17 +0200

    vds

--- Time_Memory_eval.py
+++ Time_Memory_eval.py
@@ -55,76 +55,10 @@ model = tf.keras.models.load_model('model.h5',custom_objects={"UpdatedMeanIoU": 
 print('[INFO]Generating images array')
 print('[INFO]Generating labels array')
 import time
-tempo=0;
-MB=0;
-steps=3;
-for i in range (0,steps):
-    start_time = time.time()
-    crop_images_list=[]
-    crop_labels_list=[]
-    print(i)
-    image = cv2.imread(image_list[i])[:,:,[2,1,0]]  #leggo le immagini
-    image = image.astype('float32')
-    image/=255                                    #normalizzo per avere valori per i pixel nell'intervallo [0,0.5]
-    for r in range (0,8):
-        for c in range (0,8):
-            cropped_image = image[128*r:128*(r+1),128*c:128*(c+1)]
-            crop_images_list.append(cropped_image) 
-    label = cv2.imread(label_list[i])[:,:,[2,1,0]]
-    label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
-    label=np.expand_dims(label, axis=2)  
-    label = label.astype('float32')
-    for r in range (0,8):
-        for c in range (0,8):
-            cropped_label = label[128*(r):128*(r+1),128*(c):128*(c+1)]
-            crop_labels_list.append(cropped_label)
-
-    tmp1 = crop_images_list
-    tmp2 = crop_labels_list
-
-    BATCH=1
-    x_test = tf.data.Dataset.from_tensor_slices((tmp1, tmp2))
-    x_test = (
-        x_test
-        .batch(BATCH)
-    )
-
-    print("[INFO] Starting Evaluation")
-
-    from memory_profiler import profile      #The output displays the memory consumed by each line in the code. Implementation of finding the memory consumption is very easy using a memory profiler as we directly call the decorator instead of writing a whole new code. 
-    from memory_profiler import memory_usage 
-    # instantiating the decorator            #1 MiB (mebibyte) is always 1024 Kb
-    @profile
-    def prediction(x_test,tmp2):
-        predictions = model.predict(x_test,verbose=1,steps=len(tmp2))
-        return predictions
-
-    predictions = prediction(x_test,tmp2)
-    
-    tempo += time.time() - start_time
-    print("--- %s seconds ---" % (time.time() - start_time))
-    import os, psutil
-    process = psutil.Process(os.getpid())
-    #print(process.memory_info().vms)
-    MB+=psutil.Process(os.getpid()).memory_full_info().uss / 1024 ** 2;
-    print('MB: ',psutil.Process(os.getpid()).memory_full_info().uss / 1024 ** 2)
-    #print('MB: ',psutil.Process(os.getpid()).memory_info().vms / 1024 ** 2)
-    # print('The CPU usage is: ', psutil.cpu_percent(4))
-    # print('RAM memory % used:', psutil.virtual_memory().percent)
-    
-
-tempo_medio = tempo/steps;
-MB_medio=MB/steps
-
-print('Model with cropped images-Time: ',tempo_medio)
-print('Model with cropped images-Memory (MB): ',MB_medio)
-
-
-##############RESIZE############
-# tmp1 = np.empty((1, SHAPE, SHAPE, 3), dtype=np.uint8)  #Qui ho N immagini
-# tmp2 = np.empty((1, SHAPE, SHAPE, 1), dtype=np.uint8)  #Qui ho N labels, che portano l'informazione per ogni pixel. Nel caso sparse avrò un intero ad indicare la classe
 # tempo=0;
-# steps=3;
+# MB=0;
+# steps=100;
+# isto=[];
 # for i in range (0,steps):
 #     start_time = time.time()
 #     crop_images_list=[]
@@ -133,14 +67,21 @@ print('Model with cropped images-Memory (MB): ',MB_medio)
 #     image = cv2.imread(image_list[i])[:,:,[2,1,0]]  #leggo le immagini
 #     image = image.astype('float32')
 #     image/=255                                    #normalizzo per avere valori per i pixel nell'intervallo [0,0.5]
-#     resized_image = cv2.resize(image, (SHAPE, SHAPE))
+#     for r in range (0,8):
+#         for c in range (0,8):
+#             cropped_image = image[128*r:128*(r+1),128*c:128*(c+1)]
+#             crop_images_list.append(cropped_image) 
 #     label = cv2.imread(label_list[i])[:,:,[2,1,0]]
-#     label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY) 
+#     label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
+#     label=np.expand_dims(label, axis=2)  
 #     label = label.astype('float32')
-#     resized_label = cv2.resize(label, (SHAPE, SHAPE), 0, 0, interpolation = cv2.INTER_NEAREST)
-#     resized_label=np.expand_dims(resized_label, axis=2) 
-#     tmp1[0] = resized_image
-#     tmp2[0] = resized_label
+#     for r in range (0,8):
+#         for c in range (0,8):
+#             cropped_label = label[128*(r):128*(r+1),128*(c):128*(c+1)]
+#             crop_labels_list.append(cropped_label)
+
+#     tmp1 = crop_images_list
+#     tmp2 = crop_labels_list
 
 #     BATCH=1
 #     x_test = tf.data.Dataset.from_tensor_slices((tmp1, tmp2))
@@ -152,6 +93,7 @@ print('Model with cropped images-Memory (MB): ',MB_medio)
 #     print("[INFO] Starting Evaluation")
 
 #     from memory_profiler import profile      #The output displays the memory consumed by each line in the code. Implementation of finding the memory consumption is very easy using a memory profiler as we directly call the decorator instead of writing a whole new code. 
+#     from memory_profiler import memory_usage 
 #     # instantiating the decorator            #1 MiB (mebibyte) is always 1024 Kb
 #     @profile
 #     def prediction(x_test,tmp2):
@@ -159,11 +101,94 @@ print('Model with cropped images-Memory (MB): ',MB_medio)
 #         return predictions
 
 #     predictions = prediction(x_test,tmp2)
+    
 #     tempo += time.time() - start_time
+#     isto.append(time.time() - start_time)
 #     print("--- %s seconds ---" % (time.time() - start_time))
-# tempo_medio = tempo/steps;
+#     import os, psutil
+#     process = psutil.Process(os.getpid())
+#     #print(process.memory_info().vms)
+#     MB+=psutil.Process(os.getpid()).memory_full_info().uss / 1024 ** 2;
+#     print('MB: ',psutil.Process(os.getpid()).memory_full_info().uss / 1024 ** 2)
+#     #print('MB: ',psutil.Process(os.getpid()).memory_info().vms / 1024 ** 2)
+#     # print('The CPU usage is: ', psutil.cpu_percent(4))
+#     # print('RAM memory % used:', psutil.virtual_memory().percent)
+    
 
-# print('Model with resized images: ',tempo_medio)
+# tempo_medio = tempo/steps;
+# MB_medio=MB/steps
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# plt.hist(isto,bins=100)
+# plt.show() 
+# print('Model with cropped images-Time: ',tempo_medio)
+# print('Model with cropped images-Memory (MB): ',MB_medio)
+
+
+##############RESIZE############
+tmp1 = np.empty((1, SHAPE, SHAPE, 3), dtype=np.uint8)  #Qui ho N immagini
+tmp2 = np.empty((1, SHAPE, SHAPE, 1), dtype=np.uint8)  #Qui ho N labels, che portano l'informazione per ogni pixel. Nel caso sparse avrò un intero ad indicare la classe
+tempo=0;
+MB=0;
+steps=100;
+isto=[];
+for i in range (0,steps):
+    start_time = time.time()
+    crop_images_list=[]
+    crop_labels_list=[]
+    print(i)
+    image = cv2.imread(image_list[i])[:,:,[2,1,0]]  #leggo le immagini
+    image = image.astype('float32')
+    image/=255                                    #normalizzo per avere valori per i pixel nell'intervallo [0,0.5]
+    resized_image = cv2.resize(image, (SHAPE, SHAPE))
+    label = cv2.imread(label_list[i])[:,:,[2,1,0]]
+    label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY) 
+    label = label.astype('float32')
+    resized_label = cv2.resize(label, (SHAPE, SHAPE), 0, 0, interpolation = cv2.INTER_NEAREST)
+    resized_label=np.expand_dims(resized_label, axis=2) 
+    tmp1[0] = resized_image
+    tmp2[0] = resized_label
+
+    BATCH=1
+    x_test = tf.data.Dataset.from_tensor_slices((tmp1, tmp2))
+    x_test = (
+        x_test
+        .batch(BATCH)
+    )
+
+    print("[INFO] Starting Evaluation")
+
+    from memory_profiler import profile      #The output displays the memory consumed by each line in the code. Implementation of finding the memory consumption is very easy using a memory profiler as we directly call the decorator instead of writing a whole new code. 
+    # instantiating the decorator            #1 MiB (mebibyte) is always 1024 Kb
+    @profile
+    def prediction(x_test,tmp2):
+        predictions = model.predict(x_test,verbose=1,steps=len(tmp2))
+        return predictions
+
+    predictions = prediction(x_test,tmp2)
+    tempo += time.time() - start_time
+    isto.append(time.time() - start_time)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    import os, psutil
+    process = psutil.Process(os.getpid())
+    #print(process.memory_info().vms)
+    MB+=psutil.Process(os.getpid()).memory_full_info().uss / 1024 ** 2;
+    print('MB: ',psutil.Process(os.getpid()).memory_full_info().uss / 1024 ** 2)
+    #print('MB: ',psutil.Process(os.getpid()).memory_info().vms / 1024 ** 2)
+    # print('The CPU usage is: ', psutil.cpu_percent(4))
+    # print('RAM memory % used:', psutil.virtual_memory().percent)
+
+tempo_medio = tempo/steps;
+MB_medio=MB/steps
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.hist(isto,bins=100)
+plt.show() 
+
+print('Model with resized images: ',tempo_medio)
+print('Model with resized images-Memory (MB): ',MB_medio)
 
 ###############FUNZIONE INTERA ###########
 # from memory_profiler import profile      #The output displays the memory consumed by each line in the code. Implementation of finding the memory consumption is very easy using a memory profiler as we directly call the decorator instead of writing a whole new code. 
